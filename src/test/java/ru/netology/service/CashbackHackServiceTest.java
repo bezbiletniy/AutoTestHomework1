bb5e678a0b15522063aa7456a9daf7c685c4280e
bezbiletniy <a.borodin.ca@gmail.com>
2022-08-31 15:14:42 +0300

    Add TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldLowBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldHighBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldEqualBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+}
