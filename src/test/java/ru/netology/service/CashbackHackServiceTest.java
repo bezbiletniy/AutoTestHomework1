2d72ef98452d2bdfbdba0acdf4870e1622142095
bezbiletniy <a.borodin.ca@gmail.com>
2022-08-31 15:19:16 +0300

    Add JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+
+
+    }
+}
