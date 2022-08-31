71f64c5e0f97f490f0a51f45ccd7bc246ef4c049
bezbiletniy <a.borodin.ca@gmail.com>
2022-08-31 15:21:53 +0300

    Add Junit's code

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,13 +8,35 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void shouldLowBoundaryService() {
         CashbackHackService service = new CashbackHackService();
 
         int expected = 1;
         int actual = service.remain(999);
 
+        Assert.assertEquals(expected, actual);
 
+    }
+
+    @Test
+    public void shouldHighBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldEqualBoundaryService() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(expected, actual);
 
     }
 }
